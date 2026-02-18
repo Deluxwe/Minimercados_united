96cf39501f7c39ff2435ca8ff8ccf5b37c5d78d3
Deluxwe <141252323+Deluxwe@users.noreply.github.com>
2026-02-18 13:27:00 -0500

    Add files via upload

---
+++ Claases/cConexiones.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimercados_Unidos.Claases
+{
+    internal class cConexiones
+    {
+
+        //se define la ruta de la base de datos
+        static private String CadenaConexion = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\IUE U\POO\PROYECTO POO V0.3\MINIMERCADOS_UNIDOS\MINIMERCADOS_UNIDOS\MINIMERCADOS_UNIDOS\DATABASE1.MDF;Integrated Security=True;Connect Timeout=30";
+
+        //Definir una variable para cargar la base de datos
+        private SqlConnection Conexion = new SqlConnection(CadenaConexion);
+
+        //metodo para abrir la base de datos
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+                Conexion.Open(); ;
+            return Conexion;
+        }
+        //metodo para cerrar la base de datos
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open)
+                Conexion.Close(); ;
+            return Conexion;
+        }
+
+    }
+}
