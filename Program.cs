c00943296956715d54ac15340dd74db31eb370be
Deluxwe <141252323+Deluxwe@users.noreply.github.com>
2026-02-18 13:28:57 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Minimercados_Unidos.Formularios.LogIn;
+
+namespace Minimercados_Unidos
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FmrLogIn());
+        }
+    }
+}
